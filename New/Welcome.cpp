b13ae57aeb0960785b9de22559e350a63b2658b3
Leroy <93298735+sxdleroy@users.noreply.github.com>
2025-03-12 08:54:11 +1200

    Add new C++ source files for a simple calculator and a welcome program

---
+++ New/Welcome.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    string name;
+
+    cout << "Enter your name: ";
+    cin >> name;
+    cout << "Welcome, "<<name<<"!"<<endl;
+
+    return 0;
+}
